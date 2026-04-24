143c9cb1cb5a43b92af7cef21ffbd06654773ca1
Alexander Kevin <calranderr@gmail.com>
2026-04-23 22:35:33 -0500

    Puestos

--- src/main/java/com/cibertec/poyectodawi/Controller/Puesto/PuestoController.java
+++ src/main/java/com/cibertec/poyectodawi/Controller/Puesto/PuestoController.java
@@ -7,26 +7,34 @@ import com.cibertec.poyectodawi.services.Puesto.PuestoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.PathMatcher;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.PublicKey;
 import java.util.List;
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/puestos")
+@RequestMapping("/puesto")
 
     public class PuestoController {
     private final PuestoService puestoService;
+    private final PathMatcher pathMatcher;
 
     @GetMapping()
-    public ResponseEntity<List<PuestoResponseDto>> puestos(){
-    return ResponseEntity.ok(puestoService.listarPuesto());
+    public ResponseEntity<List<PuestoResponseDto>> listar() {
+        return ResponseEntity.ok(puestoService.listarPuesto());
     }
+
     @PostMapping()
-    public ResponseEntity<PuestoResponseDto>Guardar (@RequestBody @Valid  PuestoRequestDto requestDto){
+    public ResponseEntity<PuestoResponseDto> guardar(@RequestBody @Valid PuestoRequestDto requestDto) {
         return ResponseEntity.ok(puestoService.guardar(requestDto));
-        
+
 
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<PuestoResponseDto> actualiar(@PathVariable Long id, @RequestBody PuestoRequestDto requestDto) {
 
+        return ResponseEntity.ok(puestoService.actualizar(id, requestDto));
+    }
 }
